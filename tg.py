a7a61dd80bc5fe4a7d3387bb15d60143fb214bbe
ShutenkoNazar <mrgameworld96@gmail.com>
2025-02-12 18:36:55 +0200

    Update tg.py

--- tg.py
+++ tg.py
@@ -19,51 +19,40 @@ def start(message):
     bot.send_message(message.from_user.id,"Я допоможу вам дізнаватись курс криптовалют", reply_markup=markup)
     bot.send_message(message.from_user.id,"Ціну якої валюти ви хочете дізнатись")
 
+#request func
+def reqprice(symbol, message):
+    ch24h_url = f"https://api.binance.com/api/v3/ticker/24hr?symbol={symbol}USDT"
+    ch24h_req = requests.get(ch24h_url)
+    ch24h_txt = ch24h_req.text
+    ch24h_data = json.loads(ch24h_txt)
+    change = float(ch24h_data["priceChange"])
+    change_percent = ch24h_data["priceChangePercent"]
+    change = round(change, 2)
+    change = str(change)    
+    
+    price_url = f"https://api.binance.com/api/v3/ticker/price?symbol={symbol}USDT"
+    price_response = requests.get(price_url)
+    price_data = price_response.json()
+    current_price = float(price_data["price"])
+    current_price = round(current_price, 2)
+
+    price_message = f"1 {symbol} = {current_price} USD"
+    change_message = f"За останні 24 години ціна {symbol} змінилася на {change} USD, що дорівнює {change_percent} %"
+    bot.send_message(message.from_user.id, price_message)
+    bot.send_message(message.from_user.id, change_message)
+
 
 #reply for BTC
 @bot.message_handler(func=lambda message: message.text == "BTC")
 def btc(message):
-    reqch = requests.get("https://api.binance.com/api/v3/ticker/24hr?symbol=BTCUSDT")
-    txtch = reqch.text
-    datach = json.loads(txtch)
-    change = float(datach["priceChange"])
-    changeper = datach["priceChangePercent"]
-    change = round(change, 2)
-    change = str(change)
-    req = requests.get("https://api.binance.com/api/v3/ticker/price?symbol=BTCUSDT")
-    txt = req.text
-    data = json.loads(txt)
-    bpi = float(data["price"])
-    bpi = round(bpi, 2)
-    price = "1 BTC = " + str(bpi) + " USD"
-    changemes = str("За останні 24 години ціна BTC змінилася на " + change + " USD " + " що дорівнює " + changeper + " %")
-    bot.send_message(message.from_user.id, price)
-    bot.send_message(message.from_user.id, changemes)
+    symbol = "BTC"
+    reqprice(symbol,message)
 
 #reply for ETH
 @bot.message_handler(func=lambda message: message.text == "ETH")
 def eth(message):
-    #reqest to API
-    reqch = requests.get("https://api.binance.com/api/v3/ticker/24hr?symbol=ETHUSDT")
-    txtch = reqch.text
-    #formatting json
-    datach = json.loads(txtch)
-    #select the required value (current price)
-    change = float(datach["priceChange"])
-    #select percent change
-    changeper = datach["priceChangePercent"]
-    #formatting 
-    change = round(change, 2)
-    change = str(change)
-    req = requests.get("https://api.binance.com/api/v3/ticker/price?symbol=ETHUSDT")
-    txt = req.text
-    data = json.loads(txt)
-    bpi = float(data["price"])
-    bpi = round(bpi, 2)
-    price = "1 ETH = " + str(bpi) + " USD"
-    changemes = str("За останні 24 години ціна ETH змінилася на " + change + " USD " + " що дорівнює " + changeper + " %")
-    bot.send_message(message.from_user.id, price)
-    bot.send_message(message.from_user.id, changemes)
+    symbol = "ETH"
+    reqprice(symbol, message)
 
 
 #bot infinity works
