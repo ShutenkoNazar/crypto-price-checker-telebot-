67b7bc7949d8546862376cf622dd1c7c20ff7fa4
ShutenkoNazar <mrgameworld96@gmail.com>
2025-02-12 16:00:46 +0200

    Add files via upload

---
+++ tg.py
@@ -0,0 +1,87 @@
+import telebot 
+from telebot import types
+import requests
+import json
+
+
+
+bot = telebot.TeleBot('7453386975:AAHOpXbr5dXImUWYHFepDhfFO-ktPSrZ7bk')
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    btnbtc =  types.KeyboardButton("BTC")
+    btneth = types.KeyboardButton("ETH")
+    markup.add(btneth)
+    markup.add(btnbtc)
+    bot.send_message(message.from_user.id,"Я допоможу вам дізнаватись курс криптовалют", reply_markup=markup)
+    bot.send_message(message.from_user.id,"Ціну якої валюти ви хочете дізнатись")
+
+
+
+@bot.message_handler(func=lambda message: message.text == "BTC")
+def btc(message):
+    reqch = requests.get("https://api.binance.com/api/v3/ticker/24hr?symbol=BTCUSDT")
+    txtch = reqch.text
+    datach = json.loads(txtch)
+    change = float(datach["priceChange"])
+    changeper = datach["priceChangePercent"]
+    change = round(change, 2)
+    change = str(change)
+    req = requests.get("https://api.binance.com/api/v3/ticker/price?symbol=BTCUSDT")
+    txt = req.text
+    data = json.loads(txt)
+    bpi = float(data["price"])
+    bpi = round(bpi, 2)
+    price = "1 BTC = " + str(bpi) + " USD"
+    changemes = str("За останні 24 години ціна BTC змінилася на " + change + " USD " + " що дорівнює " + changeper + " %")
+    bot.send_message(message.from_user.id, price)
+    bot.send_message(message.from_user.id, changemes)
+
+
+@bot.message_handler(func=lambda message: message.text == "ETH")
+def eth(message):
+    reqch = requests.get("https://api.binance.com/api/v3/ticker/24hr?symbol=ETHUSDT")
+    txtch = reqch.text
+    datach = json.loads(txtch)
+    change = float(datach["priceChange"])
+    changeper = datach["priceChangePercent"]
+    change = round(change, 2)
+    change = str(change)
+    req = requests.get("https://api.binance.com/api/v3/ticker/price?symbol=ETHUSDT")
+    txt = req.text
+    data = json.loads(txt)
+    bpi = float(data["price"])
+    bpi = round(bpi, 2)
+    price = "1 ETH = " + str(bpi) + " USD"
+    changemes = str("За останні 24 години ціна ETH змінилася на " + change + " USD " + " що дорівнює " + changeper + " %")
+    bot.send_message(message.from_user.id, price)
+    bot.send_message(message.from_user.id, changemes)
+
+# def btchange():
+#     reqch = requests.get("https://api.binance.com/api/v3/ticker/24hr?symbol=BTCUSDT")
+#     txtch = reqch.text
+#     datach = json.loads(txtch)
+#     change = datach["priceChange"]
+#     changeper = datach["priceChangePercent"]
+#     round(change, 2)
+
+
+
+# def handle_message(update, context):
+#     user_message = update.message.text
+#     if user_message == "BTC":
+#         btc()
+
+
+
+
+def url(message):
+    markup = types.InlineKeyboardMarkup()
+    btc = types.InlineKeyboardButton(text="BTC", url="https://www.binance.com/ru-UA/trade/BTC_USDT?type=spot")
+    eth = types.InlineKeyboardButton(text="ETH", url="https://www.binance.com/ru-UA/trade/ETH_USDT?type=spot")
+    markup.add(btc, eth)
+    bot.send_message(message.from_user.id, "Яку валюту ви хочете дізнатись?", reply_markup = markup)
+
+
+bot.polling(none_stop=True, interval=0)
